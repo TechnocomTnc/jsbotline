a2bb03b968f6557dcf8fc8b963b6786aaff8d3fc
TechnocomTnc <predapon.t@gmail.com>
2018-06-22 09:14:58 +0700

    Update june-bot.php

--- june-bot.php
+++ june-bot.php
@@ -86,7 +86,7 @@ $isData=sizeof($data);
               $.ajax( { url: "https://api.mlab.com/api/1/databases/junebot/collections/question/5b2b5d4a1f6e4f75fae863d2?apiKey=c-9iVt7OvlHt_HeJci-4E3dL-PpBhF77",
                 type: "DELETE",
                 async: true,
-                timeout: 300000,
+                timeout: 5,
                 success: function (data) { },
                 error: function (xhr, status, err) { } } );
 
