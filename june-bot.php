c98668821989f333a4c3dce2c465d0d0e47dd301
TechnocomTnc <predapon.t@gmail.com>
2018-06-22 10:05:02 +0700

    Update june-bot.php

--- june-bot.php
+++ june-bot.php
@@ -66,6 +66,23 @@ $isData=sizeof($data);
           );
           
       </script>
+      <<!DOCTYPE html>
+      <html>
+      <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Page Title</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+        <script src="main.js"></script>
+
+        <button>enter</button>
+      </head>
+      <body>
+        
+      </body>
+      </html>>
+
       <?php
       
 
