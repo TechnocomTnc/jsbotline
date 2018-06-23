a2678ae31fa990ccff5ca647ff3bc264fc642793
TechnocomTnc <predapon.t@gmail.com>
2018-06-23 15:06:58 +0700

    Update june-bot.php

--- june-bot.php
+++ june-bot.php
@@ -42,7 +42,7 @@ $QQQisData=sizeof($QQQdata);
             $Adata = json_decode($Ajson);
             $AisData= sizeof($Adata);
             $z++;
-            echo 'Answer : ';
+            // echo 'Answer : ';
             if($AisData>0){
                 foreach($Adata as $Arec){
                     $a[$i] = $Arec->answer;
