0ed04fab3c883fb0e95095409ca47b104d56cc89
TechnocomTnc <predapon.t@gmail.com>
2018-06-25 13:27:15 +0700

    Update june-bot.php

--- june-bot.php
+++ june-bot.php
@@ -104,7 +104,7 @@ $z = 0;
             echo '>'.$QQQisData.'<';
             foreach($QQQdata as $rec){
                 echo '>'.$rec->question.'<';
-                if (ereg("(.$rec->question)+",$_msg) !== false)
+                if (ereg($_msg, "(.$rec->question)+") !== false)
                 //if (strpos($_msg, $rec->question) !== false)
                 {
                     echo 'Question : ';
