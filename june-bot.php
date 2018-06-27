a673463288965ccde41bb31de284618d81197161
TechnocomTnc <predapon.t@gmail.com>
2018-06-27 13:31:07 +0700

    Update june-bot.php

--- june-bot.php
+++ june-bot.php
@@ -5,8 +5,10 @@ $arrJson = json_decode($content, true);
 $strUrl = "https://api.line.me/v2/bot/message/reply";
 $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
 // $_msg = 'หวัด';
 $_msg = $arrJson['events'][0]['message']['text'];
+
 $api_key="c-9iVt7OvlHt_HeJci-4E3dL-PpBhF77";
 $Aurl = 'https://api.mlab.com/api/1/databases/junebot/collections/AA?apiKey='.$api_key.'';
 $nonurl = 'https://api.mlab.com/api/1/databases/junebot/collections/nonQuestion?apiKey='.$api_key.'';
