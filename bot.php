2a55991f8d2543cfddec48fe9e96f02f994d91b5
TechnocomTnc <predapon.t@gmail.com>
2018-06-29 09:42:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -151,11 +151,12 @@ if($am == 'text'){
                 $uimg = $a[$b];
 
                 $image_url = "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg";
-                $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+                $arrayPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
                 $arrayPostData['messages'][0]['type'] = "image";
                 $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
                 $arrayPostData['messages'][0]['previewImageUrl'] = $image_url;
                 // replyMsg($arrayHeader,$arrayPostData);
+
 
 
 
