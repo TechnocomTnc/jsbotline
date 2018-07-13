f0d798551eb9496d21288ab3c062d477405e22b8
TechnocomTnc <predapon.t@gmail.com>
2018-07-13 11:49:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ var sql = require('mssql');
 var sqlInstance = require("mssql");
 
 
-var dbConfig = {
-  user: 'sa',
-  password: 'P@ssw0rd1234',
-  server: 'demomagic2.southeastasia.cloudapp.azure.com', 
-  database: 'LinebotDB',
-  port:1433,
-  options: {
-      encrypt: true // Use this if you're on Windows Azure
-  }
-};
+// var dbConfig = {
+//   user: 'sa',
+//   password: 'P@ssw0rd1234',
+//   server: 'demomagic2.southeastasia.cloudapp.azure.com', 
+//   database: 'LinebotDB',
+//   port:1433,
+//   options: {
+//       encrypt: true // Use this if you're on Windows Azure
+//   }
+// };
 
 
 
@@ -45,8 +45,8 @@ const app = express();
 app.use('/static', express.static('static'));
 app.use('/downloaded', express.static('downloaded'));
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json())
 
 // webhook callback
 app.post('/callback', line.middleware(config), (req, res) => {
