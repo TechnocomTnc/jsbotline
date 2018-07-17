3e0ef3bb7caad527cdfe4364281c53740cbdfa00
TechnocomTnc <predapon.t@gmail.com>
2018-07-17 15:19:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -355,7 +355,7 @@ function handleImage(message, replyToken, source) {
 
 function handleVideo(message, replyToken) {
   const downloadPath = path.join(__dirname, 'downloaded', `${message.id}.mp4`);
-  const previewPath = path.join(__dirname, 'downloaded', `${message.id}.jpg`);
+  const previewPath = path.join(__dirname, 'downloaded', `${message.id}.mp4`);
 
   return downloadContent(message.id, downloadPath)
     .then((downloadPath) => {
