35c56eb8d75a00865d87b0aaf9dd33e1524de619
TechnocomTnc <predapon.t@gmail.com>
2018-07-17 15:37:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -362,7 +362,7 @@ function handleVideo(message, replyToken) {
       // FFmpeg and ImageMagick is needed here to run 'convert'
       // Please consider about security and performance by yourself
       // cp.exec(`convert mp4:${downloadPath}[0] jpeg:${previewPath}`);
-      cp.exec(`convert jpeg:${previewPath}`); 
+      cp.exec(`convert jpeg:${downloadPath}`); 
 
       return client.replyMessage(
         replyToken,
@@ -371,7 +371,7 @@ function handleVideo(message, replyToken) {
           // originalContentUrl: 'https://r1---sn-30a7yne7.c.2mdn.net/videoplayback/id/3c2c72fb5a76d1fd/itag/343/source/doubleclick_dmm/ratebypass/yes/acao/yes/ip/0.0.0.0/ipbits/0/expire/3673239867/sparams/acao,expire,id,ip,ipbits,itag,mip,mm,mn,ms,mv,pl,ratebypass,source/signature/558883E84289FA0D99219F54D99F4376DE02191B.57F9B5616A2E2DC1E3690A466E293E27BB8BD595/key/cms1/cms_redirect/yes/mip/171.6.115.43/mm/42/mn/sn-30a7yne7/ms/onc/mt/1531807347/mv/m/pl/19/file/file.mp4',
           // previewImageUrl: 'https://r1---sn-30a7yne7.c.2mdn.net/videoplayback/id/3c2c72fb5a76d1fd/itag/343/source/doubleclick_dmm/ratebypass/yes/acao/yes/ip/0.0.0.0/ipbits/0/expire/3673239867/sparams/acao,expire,id,ip,ipbits,itag,mip,mm,mn,ms,mv,pl,ratebypass,source/signature/558883E84289FA0D99219F54D99F4376DE02191B.57F9B5616A2E2DC1E3690A466E293E27BB8BD595/key/cms1/cms_redirect/yes/mip/171.6.115.43/mm/42/mn/sn-30a7yne7/ms/onc/mt/1531807347/mv/m/pl/19/file/file-preview.jpg'
           type : 'text',
-          text : baseURL + '/downloaded/' + path.basename(previewPath)          
+          text : baseURL + '/downloaded/' + path.basename(downloadPath)          
           // text : baseURL + '/downloaded/' + path.basename(downloadPath) + '\n\n' + baseURL + '/downloaded/' + path.basename(previewPath)
           // type: 'video',
           // originalContentUrl: baseURL + '/downloaded/' + path.basename(downloadPath),
