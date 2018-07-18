89f92f9b21e9dd9f104ffa5023a241d80c354ab5
TechnocomTnc <predapon.t@gmail.com>
2018-07-18 10:34:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -302,7 +302,9 @@ function handleImage(message, replyToken, source) {
     .then((downloadPath) => {
       // ImageMagick is needed here to run 'convert'
       // Please consider about security and performance by yourself
-      cp.exec(`convert -resize 240x jpeg:${downloadPath} jpeg:${previewPath}`);
+      cp.exec(`ffmpeg. -i ${downloadPath} -vf scale=360:240 > ${previewPath}`);
+      
+      // convert -resize 240x jpeg:${downloadPath} jpeg:${previewPath}
       var  originalContentUrlT = baseURL + '/downloaded/' + path.basename(downloadPath)
       var  previewImageUrlT = baseURL + '/downloaded/' + path.basename(previewPath)
       var  UsID = source.userId
