a5e21777c104e6fe461ae47b404f90c922885c42
TechnocomTnc <predapon.t@gmail.com>
2018-07-20 13:30:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -35,28 +35,28 @@ app.post('/webhook', (req, res) => {
     let uid = req.body.events[0].source.userId
     var msID = req.body.events[0].message.id
       
-       let headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer {7YR60AJ855Zu1Etxsc7aCdFqhip1o8yAKj7PzLe90ClE9Po0fz5o81BeghtpCki4+zFZ7FrYjjbrFvQw84+Axi+P1zWPnxSCTl/lF5gVTDaDqdC5IHk30qnjo7GQ1hHKizexgGNpBPn/Fwz3slJqkQdB04t89/1O/w1cDnyilFU=}'
-        }
-        let body = JSON.stringify({
-            replyToken: reply_token,
-            messages: [{
-                    type: 'text',
-                    text: 'aa'
-                }]
-        })
-        request.post({
-            url: 'https://api.line.me/v2/bot/message/reply',
-            headers: headers,
-            body: body
-        }, (err, res, body) => {
-            console.log('status = ' + res.statusCode);
-        });  
+    //    let headers = {
+    //         'Content-Type': 'application/json',
+    //         'Authorization': 'Bearer {7YR60AJ855Zu1Etxsc7aCdFqhip1o8yAKj7PzLe90ClE9Po0fz5o81BeghtpCki4+zFZ7FrYjjbrFvQw84+Axi+P1zWPnxSCTl/lF5gVTDaDqdC5IHk30qnjo7GQ1hHKizexgGNpBPn/Fwz3slJqkQdB04t89/1O/w1cDnyilFU=}'
+    //     }
+    //     let body = JSON.stringify({
+    //         replyToken: reply_token,
+    //         messages: [{
+    //                 type: 'text',
+    //                 text: 'aa'
+    //             }]
+    //     })
+    //     request.post({
+    //         url: 'https://api.line.me/v2/bot/message/reply',
+    //         headers: headers,
+    //         body: body
+    //     }, (err, res, body) => {
+    //         console.log('status = ' + res.statusCode);
+    //     });  
 
-    // if(msgtype == 'text') {
-    //     var text = req.body.events[0].message.text
-    //     reply(reply_token, text)
+    if(msgtype == 'text') {
+        var text = req.body.events[0].message.text
+        reply(reply_token, text)
 
         // var conn = new sql.ConnectionPool(dbConfig);
         // conn.connect().then(function () {
@@ -64,7 +64,7 @@ app.post('/webhook', (req, res) => {
         //     // req.query("INSERT INTO [dbo].[groupName] ([groupID],[Gname]) VALUES ('" + gid + "','" + gid + "')")
         //     req.query("CREATE TABLE [dbo].["+ uid +"]([m_Id] [int] IDENTITY(1,1) NOT NULL,[UID] [varchar](500) NULL,[Mesg] [varchar](500) NULL)")                      
         // });
-    // }
+    }
 
     if(gid != null)
        groupMs(uid,gid,text)
@@ -153,44 +153,64 @@ function reply(reply_token, msg) {
 
 
 function groupMs(uid,gid,msg){
-    var flag,grid;
-    var conn = new sql.ConnectionPool(dbConfig);
-    conn.connect().then(function () {
-        var req = new sql.Request(conn);
-            req.query('SELECT * FROM groupName', function(err, rows) {
-                if (err) {
-                    throw err;
-                    console.error(err);
-                    conn.close();  
-                }else{
-                    for(var i=0;i<rows.rowsAffected;i++){
-                        if(rows.recordset[i].groupID == gid){
-                            var conn = new sql.ConnectionPool(dbConfig);
-                            conn.connect().then(function () {
-                                var req = new sql.Request(conn);            
-                                req.query("INSERT INTO [dbo].["+ gid +"] ([UID],[Mesg]) VALUES ('" + uid + "','" + msg + "')")
-                                })   
-                            flag = 0
-                            break
-                        }else flag = 1
-                    }
-                    if(flag == 1){
-                        var conn = new sql.ConnectionPool(dbConfig);
-                            conn.connect().then(function () {
-                                var req = new sql.Request(conn);
-                                req.query("INSERT INTO [dbo].[groupName] ([groupID],[Gname]) VALUES ('" + gid + "','" + gid + "')")
-                                req.query("CREATE TABLE [dbo].["+ gid +"]([m_Id] [int] IDENTITY(1,1) NOT NULL,[UID] [varchar](500) NULL,[Mesg] [varchar](500) NULL)")                      
-                                // req.query("INSERT INTO [dbo].["+ Ngroup +"] ([UID],[Mesg]) VALUES ('" + gid + "','" + msg + "')")
-                        });
-                        var conn = new sql.ConnectionPool(dbConfig);
-                        conn.connect().then(function () {
-                            var req = new sql.Request(conn);            
-                            req.query("INSERT INTO [dbo].["+ gid +"] ([UID],[Mesg]) VALUES ('" + gid + "','" + msg + "')")
-                    })   
-                    }
-            }
-            })
+
+   let headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer 7YR60AJ855Zu1Etxsc7aCdFqhip1o8yAKj7PzLe90ClE9Po0fz5o81BeghtpCki4+zFZ7FrYjjbrFvQw84+Axi+P1zWPnxSCTl/lF5gVTDaDqdC5IHk30qnjo7GQ1hHKizexgGNpBPn/Fwz3slJqkQdB04t89/1O/w1cDnyilFU='
+        }
+        let body = JSON.stringify({
+            replyToken: reply_token,
+            messages: [{
+                    type: 'text',
+                    text: uid
+                }]
         })
+        request.post({
+            url: 'https://api.line.me/v2/bot/message/reply',
+            headers: headers,
+            body: body
+        }, (err, res, body) => {
+            console.log('status = ' + res.statusCode);
+        });  
+
+    // var flag,grid;
+    // var conn = new sql.ConnectionPool(dbConfig);
+    // conn.connect().then(function () {
+    //     var req = new sql.Request(conn);
+    //         req.query('SELECT * FROM groupName', function(err, rows) {
+    //             if (err) {
+    //                 throw err;
+    //                 console.error(err);
+    //                 conn.close();  
+    //             }else{
+    //                 for(var i=0;i<rows.rowsAffected;i++){
+    //                     if(rows.recordset[i].groupID == gid){
+    //                         var conn = new sql.ConnectionPool(dbConfig);
+    //                         conn.connect().then(function () {
+    //                             var req = new sql.Request(conn);            
+    //                             req.query("INSERT INTO [dbo].["+ gid +"] ([UID],[Mesg]) VALUES ('" + uid + "','" + msg + "')")
+    //                             })   
+    //                         flag = 0
+    //                         break
+    //                     }else flag = 1
+    //                 }
+    //                 if(flag == 1){
+    //                     var conn = new sql.ConnectionPool(dbConfig);
+    //                         conn.connect().then(function () {
+    //                             var req = new sql.Request(conn);
+    //                             req.query("INSERT INTO [dbo].[groupName] ([groupID],[Gname]) VALUES ('" + gid + "','" + gid + "')")
+    //                             req.query("CREATE TABLE [dbo].["+ gid +"]([m_Id] [int] IDENTITY(1,1) NOT NULL,[UID] [varchar](500) NULL,[Mesg] [varchar](500) NULL)")                      
+    //                             // req.query("INSERT INTO [dbo].["+ Ngroup +"] ([UID],[Mesg]) VALUES ('" + gid + "','" + msg + "')")
+    //                     });
+    //                     var conn = new sql.ConnectionPool(dbConfig);
+    //                     conn.connect().then(function () {
+    //                         var req = new sql.Request(conn);            
+    //                         req.query("INSERT INTO [dbo].["+ gid +"] ([UID],[Mesg]) VALUES ('" + gid + "','" + msg + "')")
+    //                 })   
+    //                 }
+    //         }
+    //         })
+    //     })
 
 
 }
