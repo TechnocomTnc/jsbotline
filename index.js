a37b623e236fe8b6a0a04a6ba7c43899257c5bae
TechnocomTnc <predapon.t@gmail.com>
2018-07-23 14:38:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const path = require('path');
 var sql = require('mssql');
 const image2base64 = require('image-to-base64');
 
-var date = new Date();
-//var date = dateG0.toLocaleTimeString('en-US')
+var date2 = new Date();
+var date = date2.toLocaleTimeString('en-US');
 
 var dbConfig = {
   user: 'linebot',
