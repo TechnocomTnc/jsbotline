b514b3e9726abc502dbb3fe1398921f3ae7d5847
TechnocomTnc <predapon.t@gmail.com>
2018-07-23 09:38:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ function handleEvent(event) {
               var conn = new sql.ConnectionPool(dbConfig);
                   conn.connect().then(function () {
                       var req = new sql.Request(conn);
-                      req.query("INSERT INTO [dbo].[User] ([userId],[userName]) VALUES (" + UsID + "','" + UsName + "')")
+                      req.query("INSERT INTO [dbo].[User] ([userId],[userName]) VALUES ('" + UsID + "','" + UsName + "')")
                       return replyText(event.replyToken,"สวัสดีครับ " + UsName +" \n ผมคือระบบอัตโนมัติ บทสนทนาที่เกิดขึ้นภายในกลุ่มนี้จะถูกบันทึกเพื่อนำไปปรับปรุงและพัฒนาระบบต่อไป \nข้อมูลทุกอย่างจะถูกเก็บเป็นความลับและไม่มีการเปิดเผยต่อสาธารณะ \nขอบคุณครับ");
                 });  
                   
